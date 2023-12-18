e591f25a6e66a2c6cf37ee7134af3195baeccd49
Andrea <andrea.saitta.as@gmail.com>
2023-12-18 17:40:51 +0100

    ex01

---
+++ books.sql
@@ -0,0 +1,11 @@
+CREATE TABLE books (
+  book_id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  author TEXT NOT NULL,
+  genre TEXT NOT NULL,
+  published_year INTEGER,
+  isbn VARCHAR(13) UNIQUE,
+  price DECIMAL(10, 2),
+  rating DECIMAL(1, 1),
+  stock_count INTEGER
+)
