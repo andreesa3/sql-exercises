6ba78a879e4311485243cc8f6ba2a392d0fff53c
Andrea <andrea.saitta.as@gmail.com>
2023-12-18 18:09:55 +0100

    ex03

--- books.sql
+++ books.sql
@@ -4,12 +4,77 @@ CREATE TABLE books (
   author TEXT NOT NULL,
   genre TEXT NOT NULL,
   published_year INTEGER,
-  isbn VARCHAR(13) UNIQUE,
+  isbn VARCHAR(13),
   price DECIMAL(10, 2),
   rating DECIMAL(1, 1),
   stock_count INTEGER
 )
+ALTER TABLE
+  books
+ADD
+  COLUMN publisher TEXT,
+ADD
+  COLUMN n_pages INTEGER;
 
-ALTER TABLE books
-ADD COLUMN publisher TEXT,
-ADD COLUMN n_pages INTEGER;
+INSERT INTO
+  books (
+    title,
+    author,
+    genre,
+    published_year,
+    isbn,
+    price,
+    rating,
+    stock_count,
+    publisher,
+    n_pages
+  )
+VALUES
+  (
+    'A Journey to the Center',
+    'Jules Verne',
+    'Adventure',
+    1864,
+    '1234567890123',
+    12.99,
+    4.5,
+    10,
+    'Verne Publishing',
+    350
+  ),
+  (
+    'War and Peace',
+    'Leo Tolstoy',
+    'Historical',
+    1869,
+    '1234567890124',
+    14.99,
+    4.8,
+    5,
+    'Tolstoy Prints',
+    1200
+  ),
+  (
+    'Whispers of the Wind',
+    'Amelia Blackburn',
+    'Romance',
+    1982,
+    '1234567890125',
+    9.99,
+    4.2,
+    20,
+    'Blackburn House',
+    275
+  ),
+  (
+    'The Galactic Odyssey',
+    'Orion Starfield',
+    'Science Fiction',
+    2005,
+    '1234567890126',
+    19.99,
+    4.9,
+    15,
+    'Nebula Press',
+    450
+  );
